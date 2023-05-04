d960f55593212df1b7ee18ea386a185efa496668
PobWalerii <lstshef@gmail.com>
2023-05-04 19:16:12 +0300

    wheels

--- app/src/main/java/com/example/game/ui/game1/Game1Fragment.kt
+++ app/src/main/java/com/example/game/ui/game1/Game1Fragment.kt
@@ -4,18 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
-import android.widget.FrameLayout
-import android.widget.ImageView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import com.example.game.R
 import com.example.game.databinding.FragmentGame1Binding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import java.util.*
 
 
 @AndroidEntryPoint
@@ -36,10 +31,24 @@ class Game1Fragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initWheels()
         observeGamersBalance()
         observeRateChange()
         rateKeysListener()
         clickAndRotete()
+    }
+
+    private fun initWheels() {
+        for(i in 1..3) {
+            val random = Random()
+            val startImage = random.nextInt(viewModel.list_images.size) + 1
+
+
+
+
+
+
+        }
     }
 
     private fun observeGamersBalance() {
@@ -70,7 +79,9 @@ class Game1Fragment : Fragment() {
 
     private fun clickAndRotete() {
         binding.splinInclude.splin.setOnClickListener {
+            startScrolling()
 
+            /*
             Toast.makeText(context,"Rotate",Toast.LENGTH_SHORT).show()
 
             val wheelContainer: FrameLayout = binding.wheel1.wheelContainer
@@ -85,9 +96,42 @@ class Game1Fragment : Fragment() {
                 wheel.clearAnimation()
             }
 
+             */
+
 
 
         }
+    }
+
+    private fun startScrolling() {
+
+        /*
+        val scrollSpeed = 10
+        val imageWidth = 100 // ширина картинки
+        val imageHeight = 100 // высота картинки
+        val screenHeight = binding.root.height
+        val imageViews = listOf(binding.imageView1, binding.imageView2, binding.imageView3)
+
+        // Инициализация списка картинок
+        imageViews.forEachIndexed { index, imageView ->
+            imageView.setImageResource(R.drawable.image_${index + 1})
+            imageView.y = index * imageHeight.toFloat()
+        }
+
+        // Запуск бесконечного цикла прокрутки
+        while (true) {
+            for (imageView in imageViews) {
+                imageView.y += scrollSpeed
+                if (imageView.y > screenHeight) {
+                    val topImageView = imageViews.first()
+                    topImageView.y = imageViews.last().y - imageHeight
+                    Collections.rotate(imageViews, -1)
+                }
+            }
+            Thread.sleep(20)
+        }
+
+         */
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/game/ui/game1/Game1ViewModel.kt
+++ app/src/main/java/com/example/game/ui/game1/Game1ViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.game.ui.game1
 
 import androidx.lifecycle.ViewModel
+import com.example.game.R
 import com.example.game.repository.GameRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.StateFlow
@@ -13,6 +14,15 @@ class Game1ViewModel @Inject constructor(
 
     val gamerBalance: StateFlow<Int> = gameRepository.gamerBalance
     val gamerRate: StateFlow<Int> = gameRepository.gamerRateGame1
+    val list_images: List<Int> = listOf(
+        R.drawable.game1_slot1,
+        R.drawable.game1_slot2,
+        R.drawable.game1_slot3,
+        R.drawable.game1_slot4,
+        R.drawable.game1_slot5,
+        R.drawable.game1_slot6,
+        R.drawable.game1_slot7,
+    )
 
     init {
         gameRepository.startRateGame1()
